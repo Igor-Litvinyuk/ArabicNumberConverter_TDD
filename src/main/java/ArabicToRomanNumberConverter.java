34979a8d55c293b184d13cf5203134ab97de9591
Igor <igorlitvinyuk1993@gmail.com>
2018-05-04 11:45:25 +0300

    Refactoring

--- src/main/java/ArabicToRomanNumberConverter.java
+++ src/main/java/ArabicToRomanNumberConverter.java
@@ -1,14 +1,38 @@
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArabicToRomanNumberConverter {
 
+    private List<Pair<Integer, String>> map;
+
+    public ArabicToRomanNumberConverter() {
+        map = new ArrayList<>();
+        map.add(new Pair<>(5, "V"));
+        map.add(new Pair<>(1, "I"));
+    }
+
     public String convert(int arabicNumber) {
-        if (arabicNumber == 0){
-            return "";
+        boolean noNumber = arabicNumber == 0;
+        if (noNumber){
+            return getDefaultRomanNumber();
         }
-        else if (arabicNumber == 1){
-            return "I";
+        return getRepeatingRomanNumber(arabicNumber);
+    }
+
+    private String getRepeatingRomanNumber(int arabicNumber) {
+        String romanNumber = "";
+        for (Pair<Integer, String> digit : map){
+            while (arabicNumber >= digit.getKey()){
+                romanNumber += digit.getValue();
+                arabicNumber -= digit.getKey();
+            }
         }
-        else {
-            return "II";
-        }
+        return romanNumber;
+    }
+
+    private String getDefaultRomanNumber() {
+        return "";
     }
 }
--- src/test/java/ArabicToRomanNumbersConverterTest.java
+++ src/test/java/ArabicToRomanNumbersConverterTest.java
@@ -19,6 +19,31 @@ public class ArabicToRomanNumbersConverterTest {
         assertEquals("II", convertToRoman(2));
     }
 
+    @Test
+    public void convert_3_To_III(){
+        assertEquals("III", convertToRoman(3));
+    }
+
+    @Test
+    public void convert_5_To_V(){
+        assertEquals("V", convertToRoman(5));
+    }
+
+    @Test
+    public void convert_6_To_VI(){
+        assertEquals("VI", convertToRoman(6));
+    }
+
+    @Test
+    public void convert_7_To_VII(){
+        assertEquals("VII", convertToRoman(7));
+    }
+
+    @Test
+    public void convert_8_To_VIII(){
+        assertEquals("VIII", convertToRoman(8));
+    }
+
     private String convertToRoman(int arabicNumber) {
         ArabicToRomanNumberConverter converter = new ArabicToRomanNumberConverter();
         return converter.convert(arabicNumber);
