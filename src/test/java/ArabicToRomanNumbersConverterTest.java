24c907e15697a0e04d6c7569c522cb55a5def774
Igor <igorlitvinyuk1993@gmail.com>
2018-05-04 11:04:23 +0300

    After refactoring

--- src/test/java/ArabicToRomanNumbersConverterTest.java
+++ src/test/java/ArabicToRomanNumbersConverterTest.java
@@ -6,22 +6,21 @@ public class ArabicToRomanNumbersConverterTest {
 
     @Test
     public void convert_0_To_EmptyString(){
-        ArabicToRomanNumberConverter converter = new ArabicToRomanNumberConverter();
-        String romanNumber = converter.convert(0);
-        assertEquals("", romanNumber);
+        assertEquals("", convertToRoman(0));
     }
 
     @Test
     public void convert_1_To_I(){
-        ArabicToRomanNumberConverter converter = new ArabicToRomanNumberConverter();
-        String romanNumber = converter.convert(1);
-        assertEquals("I", romanNumber);
+        assertEquals("I", convertToRoman(1));
     }
 
     @Test
     public void convert_2_To_II(){
+        assertEquals("II", convertToRoman(2));
+    }
+
+    private String convertToRoman(int arabicNumber) {
         ArabicToRomanNumberConverter converter = new ArabicToRomanNumberConverter();
-        String romanNumber = converter.convert(2);
-        assertEquals("II", romanNumber);
+        return converter.convert(arabicNumber);
     }
 }
